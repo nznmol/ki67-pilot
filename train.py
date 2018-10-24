02aef8c3e2e27a49d4e8b318233d27a961012c78
Nazanin Mola <pr.nazanin@gmail.com>
2018-10-24 20:23:45 +0200

    Add function for counting net and data parameters
    Closes #3

--- train.py
+++ train.py
@@ -4,14 +4,36 @@ import os
 from os import path
 import tensorflow as tf
 from functools import reduce
+import numpy as np
+from scipy import misc
+import glob
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')
 
 #count variables
 def count_params():
     "print number of trainable variables"
     size = lambda v: reduce((lambda x, y: x*y), v.get_shape().as_list())
     n = sum(size(v) for v in tf.trainable_variables())
-    print("Model size: %dK" % (n/1000,))
+    logging.info("Model size: %dk" % (n/1000,))
 
+def count_relevant_pixels():
+    data_root = os.getenv("DATA_ROOT", os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), "data"))
+    train_data = path.join(data_root, "train", "*_mask.tif")
+    pixels = 0
+    for img_path in sorted(glob.glob(train_data)):
+        file = path.basename(img_path)
+        mask = np.fromfile(img_path, dtype=np.bool)
+        nonzero_indices = mask.nonzero()
+        ratio = 1 - len(np.flatnonzero(mask))/float(mask.size)
+        pixels = 768*768*ratio
+    logging.info("Relevant pixels in training data: %dk " % (pixels/1000))
+
+def count():
+    count_params()
+    count_relevant_pixels()
 
 #preparing data loading
 def prepare_data(data_root):
@@ -50,7 +72,7 @@ def main():
     net = setup_network(layers, features)
     data_root = os.getenv("DATA_ROOT", os.path.join(
         os.path.dirname(os.path.realpath(__file__)), "data"))
-    count_params()
+    count()
     if os.getenv("TRAIN") != None:
         data_provider = prepare_data(data_root)
         model_path = train(data_provider, net)
